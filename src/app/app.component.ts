2f930aa8a1235608d0d0ff485c6290dc3abb1877
Rohan Vasoya <rohanvasoya@Rohans-MacBook-Air.local>
2023-09-20 17:47:45 +0530

    5:47

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Drag_Drop';
+  constructor() {}
+  file: any;
+
+  getFile(event: any) {
+    this.file = event.target.files[0];
+    console.log('file', this.file);
+  }
 }
